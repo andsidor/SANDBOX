e0f47d16784e03d4087d14329b2cde0751d262ae
asidor <asidor23@gmail.com>
2023-01-10 23:02:04 +0100

    update 2

---
+++ COBOL/FirstProgram.cbl
@@ -0,0 +1,14 @@
+       program-id. FirstProgram.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+       linkage section.
+
+       procedure division.
+           
+           goback.
+
+       end program FirstProgram.
