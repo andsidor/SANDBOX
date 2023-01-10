53dc5156d661b931944c6305bdffc9d911a1286f
asidor <asidor23@gmail.com>
2023-01-11 00:28:49 +0100

    Next Up

--- COBOL/FirstProgram.cbl
+++ COBOL/FirstProgram.cbl
@@ -5,10 +5,21 @@
 
        data division.
        working-storage section.
+           77 X pic 99.
+           77 Y pic 99.
+           77 Z pic 99.
        linkage section.
 
        procedure division.
-           display "message"
+           display "message2dfdfgdfg"
+           set X to 10.
+           set y to 25.
+           add X Y GIVING Z
+           display "X"X
+           display "Y"Y]
+           DISPLAY "X + Y = "Z
+
+
            goback.
 
        end program FirstProgram.
