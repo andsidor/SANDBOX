fc316fbb52c73eaa1a5118eb5737fefb7807010f
asidor <asidor23@gmail.com>
2023-01-11 02:38:13 +0100

    new basic

---
+++ COBOL/BasicOperation/basic.cbl
@@ -0,0 +1,17 @@
+       program-id. basic1.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+       linkage section.
+
+       procedure division.
+           
+
+
+           display "TEST"
+           goback.
+
+       end program basic1.
